7ecabd17f4cd01d35a7e3187b6cdb28f030122fe
Yoshio <yoshiokiyamusu@gmail.com>
2022-01-18 17:39:13 -0500

    first commit

--- dist/controllers/weather.js
+++ dist/controllers/weather.js
@@ -5,7 +5,7 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.createWeatherItem = exports.writeDateTemp = void 0;
+exports.getWeatheRecords = exports.createWeatherItem = exports.writeDateTemp = void 0;
 
 var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
 
@@ -17,13 +17,18 @@ var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/
 
 var _database = require("../database.js");
 
-var _nodeFetch = _interopRequireDefault(require("node-fetch"));
+var _nextAbsoluteUrl = _interopRequireDefault(require("next-absolute-url"));
 
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) { symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); } keys.push.apply(keys, symbols); } return keys; }
 
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { (0, _defineProperty2["default"])(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
-//const fetch = require('node-fetch')
+//const fetch = require("node-fetch");
+var fetch = require('node-fetch');
+
+//import fetch from 'node-fetch';
+var variable_http_end = 'http://localhost:3320'; //'https://api.herokuapp.com';
+
 var writeDateTemp = /*#__PURE__*/function () {
   var _ref = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee(req, res) {
     var url_weather_api_free;
@@ -31,13 +36,48 @@ var writeDateTemp = /*#__PURE__*/function () {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            url_weather_api_free = 'api.openweathermap.org/data/2.5/forecast?q=trumbull&appid=136a856c1b0889b94d1483e842ebb43b';
+            url_weather_api_free = 'https://api.openweathermap.org/data/2.5/forecast?q=bridgeport&appid=136a856c1b0889b94d1483e842ebb43b';
             _context.next = 3;
-            return (0, _nodeFetch["default"])(url_weather_api_free, {//headers: { Authorization: 'Bearer ' + token }
+            return fetch(url_weather_api_free, {//headers: { Authorization: 'Bearer ' + token }
             }).then(function (rows) {
               return rows.json();
             }).then(function (data) {
-              data.list[0].weather;
+              //console.log(data);
+              //console.log(data.list.length);
+              var n = 0;
+
+              for (n = 0; n < data.list.length; n++) {
+                /*
+                console.log(data.list[n].main.temp); //data
+                console.log(data.list[n].main.feels_like); 
+                console.log(data.list[n].dt_txt); 
+                console.log(data.list[n].weather[0].description);
+                console.log(n);
+                */
+                //Insert tabla mysql Orden despacho         
+                fetch(variable_http_end + '/weather/reg_weather_item', {
+                  method: "POST",
+                  body: JSON.stringify({
+                    temp_day: data.list[n].main.temp,
+                    feels_like: data.list[n].main.feels_like,
+                    date_txt: data.list[n].dt_txt
+                  }),
+                  headers: {
+                    "Content-Type": "application/json" //Authorization: "Bearer " + token,
+
+                  }
+                }).then(function (rows) {//console.log(rows);
+                })["catch"](function (err) {
+                  console.log(err);
+                });
+              } //End for
+
+
+              //End for
+              var newBlog = {
+                status: 'ok'
+              };
+              res.json(newBlog);
             })["catch"](function (err) {
               console.log(err);
             });
@@ -65,17 +105,16 @@ var createWeatherItem = /*#__PURE__*/function () {
       while (1) {
         switch (_context2.prev = _context2.next) {
           case 0:
-            console.log(req.body.weather_id);
-            _context2.prev = 1;
-            _context2.next = 4;
+            _context2.prev = 0;
+            _context2.next = 3;
             return (0, _database.connect)();
 
-          case 4:
+          case 3:
             connection = _context2.sent;
-            _context2.next = 7;
+            _context2.next = 6;
             return connection.query("INSERT INTO weather (temp_day, feels_like, date_txt) VALUES (?, ?, ?)", [req.body.temp_day, req.body.feels_like, req.body.date_txt]);
 
-          case 7:
+          case 6:
             _yield$connection$que = _context2.sent;
             _yield$connection$que2 = (0, _slicedToArray2["default"])(_yield$connection$que, 1);
             results = _yield$connection$que2[0];
@@ -84,20 +123,20 @@ var createWeatherItem = /*#__PURE__*/function () {
               id: results.insertId
             }, req.body);
             res.json(newBlog);
-            _context2.next = 18;
+            _context2.next = 17;
             break;
 
-          case 15:
-            _context2.prev = 15;
-            _context2.t0 = _context2["catch"](1);
+          case 14:
+            _context2.prev = 14;
+            _context2.t0 = _context2["catch"](0);
             console.error(_context2.t0);
 
-          case 18:
+          case 17:
           case "end":
             return _context2.stop();
         }
       }
-    }, _callee2, null, [[1, 15]]);
+    }, _callee2, null, [[0, 14]]);
   }));
 
   return function createWeatherItem(_x3, _x4) {
@@ -106,3 +145,40 @@ var createWeatherItem = /*#__PURE__*/function () {
 }();
 
 exports.createWeatherItem = createWeatherItem;
+
+var getWeatheRecords = /*#__PURE__*/function () {
+  var _ref3 = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee3(req, res) {
+    var connection, _yield$connection$que3, _yield$connection$que4, rows;
+
+    return _regenerator["default"].wrap(function _callee3$(_context3) {
+      while (1) {
+        switch (_context3.prev = _context3.next) {
+          case 0:
+            _context3.next = 2;
+            return (0, _database.connect)();
+
+          case 2:
+            connection = _context3.sent;
+            _context3.next = 5;
+            return connection.query("SELECT * from weather");
+
+          case 5:
+            _yield$connection$que3 = _context3.sent;
+            _yield$connection$que4 = (0, _slicedToArray2["default"])(_yield$connection$que3, 1);
+            rows = _yield$connection$que4[0];
+            res.json(rows);
+
+          case 9:
+          case "end":
+            return _context3.stop();
+        }
+      }
+    }, _callee3);
+  }));
+
+  return function getWeatheRecords(_x5, _x6) {
+    return _ref3.apply(this, arguments);
+  };
+}();
+
+exports.getWeatheRecords = getWeatheRecords;
--- dist/routes/weather.js
+++ dist/routes/weather.js
@@ -13,8 +13,9 @@ var isAuth = require("../middleware/is-auth"); //para ponerle restriccion de tok
 
 
 var router = (0, _express.Router)();
-router.get('/writeDateTemp', _weather.writeDateTemp); //isAuth, 
+router.post('/writeDateTemp', _weather.writeDateTemp); //isAuth, 
 
 router.post('/reg_weather_item', _weather.createWeatherItem);
+router.get('/get_stored_weather', isAuth, _weather.getWeatheRecords);
 var _default = router;
 exports["default"] = _default;
